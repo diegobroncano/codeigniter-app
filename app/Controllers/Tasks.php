351f3c8d44dac71f84267f224c3111018a5d2c98
Diego Hernández <diego@hernandezf.com>
2021-08-05 20:46:07 +0200

    Add show function in Task controller

--- app/Controllers/Tasks.php
+++ app/Controllers/Tasks.php
@@ -3,15 +3,24 @@
 
 namespace App\Controllers;
 
+use \App\Models\TaskModel;
 
 class Tasks extends BaseController
 {
 	public function index()
 	{
-		$model = new \App\Models\TaskModel;
+		$model = new TaskModel;
 		$data_to_view = $model->findAll();
 
-
 		return view("Tasks/index", ['tasks' => $data_to_view]);
+	}
+
+	public function show($id)
+	{
+		$model = new TaskModel;
+
+		$task = $model->find($id);
+
+		return view( "Tasks/show", ['task' => $task] );
 	}
 }
---
+++ app/Views/Tasks/show.php
@@ -0,0 +1,24 @@
+<?php /** @var array $task */ ?>
+<?= $this->extend('layouts/default'); ?>
+
+<?= $this->section('title'); ?><?= $task['description']; ?><?= $this->endSection(); ?>
+
+<?= $this->section('content'); ?>
+
+	<h1><?= $task['description']; ?></h1>
+
+	<dl>
+		<dt>ID</dt>
+		<dd><?= $task['id']; ?></dd>
+
+		<dt>Description</dt>
+		<dd><?= $task['description']; ?></dd>
+
+		<dt>Created at</dt>
+		<dd><?= $task['created_at']; ?></dd>
+
+		<dt>Updated at</dt>
+		<dd><?= $task['updated_at']; ?></dd>
+	</dl>
+
+<?= $this->endSection(); ?>
