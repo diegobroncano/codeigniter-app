2b49fe8dd3425d239a11b569517ab42840d2ce79
Diego Hernández <diego@hernandezf.com>
2021-08-18 23:19:23 +0200

    FIX: Allow strings as flash messages.

--- app/Views/partials/flash-messages.php
+++ app/Views/partials/flash-messages.php
@@ -7,9 +7,13 @@ foreach ( $flash_types as $type ) {
 	if( session()->has($type) ): ?>
 		<div class="<?= $type ?>">
 			<ul>
-				<?php foreach (session($type) as $item ): ?>
-					<li><?= $item ?></li>
-				<?php endforeach; ?>
+				<?php if ( is_string(session($type)) ): ?>
+					<li><?= session($type); ?></li>
+				<?php else: ?>
+					<?php foreach (session($type) as $item ): ?>
+						<li><?= $item; ?></li>
+					<?php endforeach; ?>
+				<?php endif; ?>
 			</ul>
 		</div>
 	<?php endif;
